ed84399ac7f25d473f6014e24fc38471bf623c5b
wnstn819 <wnstn819@naver.com>
2024-02-01 20:41:46 +0900

    🐛️Fix : sql sqlfluff convention 수정 - data.sql, schema.sql lint 수정 -> 소, 대문자, 빈칸 등

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE if NOT exists onboarding(board_id serial PRIMARY KEY, question VARCHAR(255));
+CREATE TABLE IF NOT EXISTS onboarding (board_id SERIAL PRIMARY KEY, question varchar(255));
 
-CREATE TABLE if NOT exists choice(id serial PRIMARY KEY, board_id integer, text VARCHAR(255));
+CREATE TABLE IF NOT EXISTS choice (id SERIAL PRIMARY KEY, board_id integer, text varchar(255));
 
-CREATE TABLE if NOT exists keyword(id serial PRIMARY KEY, text VARCHAR(255));
+CREATE TABLE IF NOT EXISTS keyword (id SERIAL PRIMARY KEY, text varchar(255));
 
-CREATE TABLE if NOT exists member(id serial PRIMARY KEY, email VARCHAR(255),
-    password INTEGER, image VARCHAR(255), nickname VARCHAR(255),
-    type VARCHAR(255), enroll_date Date, update_date Date
+CREATE TABLE IF NOT EXISTS member (id SERIAL PRIMARY KEY, email varchar(255),
+    password integer, image varchar(255), nickname varchar(255),
+    type varchar(255), enroll_date date, update_date date
 );
 
-CREATE TABLE if NOT exists member_keyword(id serial PRIMARY KEY, member_id integer,  keyword VARCHAR(255));
+CREATE TABLE IF NOT EXISTS member_keyword (id SERIAL PRIMARY KEY, member_id integer, keyword varchar(255));
 
-TRUNCATE TABLE onboarding RESTART IDENTITY ;
+TRUNCATE TABLE onboarding RESTART IDENTITY;
 TRUNCATE TABLE choice RESTART IDENTITY;
 TRUNCATE TABLE keyword RESTART IDENTITY;
