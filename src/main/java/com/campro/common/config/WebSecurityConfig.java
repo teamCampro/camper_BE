616333cdb437ecc788338618ed6805363de4b9d2
crispin <shoy1415@gmail.com>
2024-02-01 12:13:31 +0900

    :wrench: Config: spring security setting #20
      - 스프링 시큐리티 설정 파일 추가

---
+++ src/main/java/com/campro/common/config/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.campro.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+        return httpSecurity
+                .csrf(csrf ->
+                        csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                .ignoringRequestMatchers(
+                                        "/h2-console/**",
+                                        "/static/**"
+                                )
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                mvcMatcherBuilder.pattern("/error"),
+                                mvcMatcherBuilder.pattern("/api/login"),
+                                mvcMatcherBuilder.pattern("/api/signup")
+                        )
+                        .permitAll()
+                        .requestMatchers(
+                                AntPathRequestMatcher.antMatcher("/static/**"),
+                                AntPathRequestMatcher.antMatcher("/api/main/**"),
+                                AntPathRequestMatcher.antMatcher("/api/oauth2/**"),
+                                AntPathRequestMatcher.antMatcher("/h2-console/**"))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000"
+        ));
+        configuration.setAllowedMethods(List.of(
+                GET.name(),
+                POST.name(),
+                DELETE.name(),
+                OPTIONS.name()
+        ));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
